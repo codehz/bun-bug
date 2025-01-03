2d8332479ce739e8dba47be8f41125936fec20f3
Neko Hz <git@hertz.moe>
2025-01-03 11:01:35 +0000

    add workaround

--- ptr.c
+++ ptr.c
@@ -2,6 +2,10 @@ struct X {
   char *buffer;
 };
 
+void *workaround(void *a) {
+  return a;
+}
+
 struct X *getInstance() {
   static struct X x = {};
   x.buffer = __builtin_malloc(10);
--- ptr.ts
+++ ptr.ts
@@ -2,9 +2,13 @@ import { cc, read, toBuffer, type Pointer } from "bun:ffi";
 
 const { symbols } = cc({
   source: "./ptr.c",
-  symbols: { getInstance: { args: [], returns: "ptr" } },
+  symbols: {
+    getInstance: { args: [], returns: "ptr" },
+    workaround: { args: ["usize"], returns: 'ptr' }
+  } as const,
 });
 
 const x = symbols.getInstance();
 const data = read.ptr(x!, 0);
+console.log(toBuffer(symbols.workaround(data)!, 0, 10))
 console.log(toBuffer(data as Pointer, 0, 10))
